a247477cd02f93c78f76715d15fedbb7f78ecbda
kondrat1206 <kondrat1206@gmail.com>
2023-07-27 14:48:08 +0300

    123

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def total(a=5, *golova, **phone_book):
         print('single_item', single_item)
 
     #проход по всем элементам словаря
-    for first_part, second_part in phone_book.items():
-        print(first_part,second_part)
+    for firsct_part, second_part in phone_book.items():
+        print(firsct_part,second_part)
 
-print(total(10, 1, 2, 3, 4, Jack=1123, John=2231, Inge=1560))
+total(10, 1, 2, 3, 4, 'a', Jack=1123, John=2231, Inge=1560)
