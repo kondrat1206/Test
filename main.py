a664e30d50dab2d872a71cd2a36cebd3db82f624
kondrat1206 <kondrat1206@gmail.com>
2023-08-03 12:35:14 +0300

    111

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-base_rate = 40
-price_per_km = 10
-total_trip = 0
-
-
-
-def calculate_trip_price(distance_km):
-    global total_trip
-    price = base_rate + price_per_km * distance_km
-    total_trip += 1
-    return price
-
-price = calculate_trip_price(5)
-print(price)
-print(total_trip)
+message = list('hello')
+            
+print(message[-3::1])
